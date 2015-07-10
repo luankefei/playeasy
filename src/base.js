43916c82157e9d8da5ffa7cc7994db6526ba3704
luankefei <luankefei@gmail.com>
2015-07-10 14:53:24 +0800

    update base module: add initUserMenu

--- src/base.js
+++ src/base.js
@@ -10,15 +10,37 @@ var PE = (function() {
 
     var pe = {}
 
+    // 初始化用户列表
+    var initUserMenu = function() {
+
+        var user = window.currentUser
+
+        if (user !== undefined) {
+
+            // TODO: 这个id没必要
+            var name = $('#user-name')
+            var accountList = $('#account')
+
+            name.html(user.username)
+            accountList.show()
+        }
+    }
+
     // 从session中获取当前用户
     pe.getCurrentUser = function() {
 
-        $.get('/session', function(user) {
+        $.get('/session', function(d) {
 
-            console.log(user)
+            if (d.code == 0)
+
+            window.currentUser = d.data
+
+            // 初始化用户菜单
+            initUserMenu()
         })
     }
 
     return pe
 
 } ())
+
