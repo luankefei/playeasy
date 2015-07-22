91686b4096ede86a247daea85fb01b5e5d2bdc3b
luankefei <luankefei@gmail.com>
2015-07-22 14:21:54 +0800

    增加全局配置文件setting.js

---
+++ src/setting.js
@@ -0,0 +1,25 @@
+
+'use strict'
+
+/**
+ * @name  pe.js
+ * @description  全局js，主要负责声明配置项
+ * @author  sunken
+ * @date 2015.7.22
+ */
+const SETTING = {}
+
+// 图表相关配置
+SETTING.chart = {
+
+    supportsType: [
+        'line', 
+        'spline', 
+        'area', 
+        'areaspline', 
+        'column', 
+        'bar', 
+        'pie', 
+        'scatter'
+    ]
+}
